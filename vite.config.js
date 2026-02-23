7245dc4b8fbfd07fc186b6c0bd0bb30eed5f754f
ricky-keys <sandjon.ricky@gmail.com>
2026-02-23 23:14:52 +0100

    Finalisation des optimisations et correction des images

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: './', // Utilise './' pour que les liens fonctionnent partout sans prise de tête
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    // Si on lance 'npm run dev', la base est '/', sinon c'est '/portfolio/'
+    base: command === 'serve' ? '/' : '/portfolio/',
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          },
         },
       },
+      chunkSizeWarningLimit: 1000,
     },
-    chunkSizeWarningLimit: 1000,
-  },
+  }
 })
