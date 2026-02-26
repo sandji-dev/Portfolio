f3f4beed316c35f8842afd0b919b8273bf3b8b6f
ricky-keys <sandjon.ricky@gmail.com>
2026-02-26 20:07:49 +0100

    fix: set base to root for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    // Si on lance 'npm run dev', la base est '/', sinon c'est '/portfolio/'
-    base: command === 'serve' ? '/' : '/Portfolio/',
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return id.toString().split('node_modules/')[1].split('/')[0].toString();
-            }
-          },
+// https://vitejs.dev
+export default defineConfig({
+  plugins: [react()],
+  // IMPORTANT : Pour Vercel, on utilise toujours la racine '/'
+  base: '/', 
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
         },
       },
-      chunkSizeWarningLimit: 1000,
     },
-  }
+    chunkSizeWarningLimit: 1000,
+  },
 })
